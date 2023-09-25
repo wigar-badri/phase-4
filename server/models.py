09d2a3debfce5d412213d48e4344d95c24f2d87d
wigarbadri <wigarbadri@gmail.com>
2023-09-25 14:20:10 -0400

    post models

--- server/models.py
+++ server/models.py
@@ -13,7 +13,7 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -21,14 +21,43 @@ class User(db.Model):
     last_name= db.Column(db.String, nullable=False)
     level = db.Column(db.Integer, nullable=False)
 
-class Post(db.Model):
-    pass
+    #relationship 
+    savedPosts = db.relationship('savedPost', back_populates='user')
+    posts = association_proxy('savedPosts', 'post')
 
+    #serialization rules
+    serialize_rules = ('-savedPosts.user',)
+class Post(db.Model, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String)
+    year_published =  db.Column(db.String)
+
+    #relationships
+    savedPosts = db.relationship ('savedPost', back_populates='post')
+    users = association_proxy('savedPosts', 'user')
+
+    #serialization rules
+    serialize_rules = ('-savedPosts.post',)
 class Stock(db.Model):
     pass
 
-class savedPost(db.Model):
-    pass
+class savedPost(db.Model, SerializerMixin):
+    __tablename__ = 'savedPosts'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    user = db.relationship('User', back_populates='savedPosts')
+    post = db.relationship('Post', back_populates='savedPosts')
+
+    #serialization rules
+    serialize_rules = ('-user.appearances', '-post.appearances')
 
 class Trade(db.Model):
     pass
