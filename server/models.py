97666c26128f3a23eb7ee1cb3d87e3923eac7bb9
wigarbadri <wigarbadri@gmail.com>
2023-09-25 11:58:54 -0400

    model setup

---
+++ server/models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name= db.Column(db.String, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+
+class Post(db.Model):
+    pass
+
+class Stock(db.Model):
+    pass
+
+class savedPost(db.Model):
+    pass
+
+class Trade(db.Model):
+    pass
