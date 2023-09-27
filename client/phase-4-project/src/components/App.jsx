9b60c94b19a75a07d293fa144bc2988bdb71899c
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-09-27 16:10:31 -0400

    app component

---
+++ client/phase-4-project/src/components/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <div>
+        <h3>Page content</h3>
+      </div>
+      <footer>
+        <p>client side ready to use</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
