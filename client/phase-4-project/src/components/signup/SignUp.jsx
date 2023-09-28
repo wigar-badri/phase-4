a5b666a92b857f698b28756396421435c2f0c415
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-09-28 10:20:42 -0400

    create login template

---
+++ client/phase-4-project/src/components/signup/SignUp.jsx
@@ -0,0 +1,23 @@
+// Basic SingUp template //
+function SignUp ({props}) {
+    
+
+    return (
+        <div className="SignUp-Page">    
+            <div className="SignUp-form">
+                <input type="text" name="" id="" />
+                <input type="text" name="" id="" />
+                <input type="text" name="" id="" />
+                <input type="text" name="" id="" />
+                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" />
+                <input type="button" value="" />
+            </div>
+            <footer className="SignUp-disclaimer">
+                <p>Random disclaimer message. 2023 registered company</p>
+            </footer>
+        </div>
+    )
+}
+
+export default SignUp;
