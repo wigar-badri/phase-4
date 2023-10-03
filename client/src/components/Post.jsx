67755ef776ac0fad29aac9d6fbeea8994d750903
Larry Tapia <tapialarry1@gmail.com>
2023-10-03 14:01:19 -0400

    Added Post

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,8 +1,16 @@
-
+import React from "react"
+import { useLoaderData} from "react-router-dom"
 
 export default function Post() {
 
+	const post = useLoaderData()
+
 	return (
-		<></>
+		<div>
+			<h1>{post.title}</h1>
+			<h3>{post.author}</h3>
+			<h3>{post.year_publised}</h3>
+			<p>{post.content}</p>
+		</div>
 	)
 }
--- client/src/components/PostMini.jsx
+++ client/src/components/PostMini.jsx
@@ -1,8 +1,12 @@
 
 
-export default function PostMini() {
+export default function PostMini({post}) {
 
 	return (
-		<></>
+		<div className = "miniPost">
+			<h3>{post.title}</h3>
+			<p>{post.author}</p>
+			<p>{post.year_published}</p>
+		</div>
 	)
 }
