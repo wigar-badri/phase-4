bd2790c91eb9a76127be4575ba11ecd4a0861551
Larry Tapia <tapialarry1@gmail.com>
2023-10-03 16:28:33 -0400

    Final Post Stuff 3

--- client/src/components/PostMini.jsx
+++ client/src/components/PostMini.jsx
@@ -1,10 +1,12 @@
-
+import { Link } from 'react-router-dom'
 
 export default function PostMini({post}) {
 
 	return (
 		<div className = "miniPost">
-			<h3>{post.title}</h3>
+			<Link to = {`/posts/${post.id}`}>
+				<h3>{post.title}</h3>
+			</Link>
 			<p>{post.author}</p>
 			<p>{post.year_published}</p>
 		</div>
